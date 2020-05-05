5744301fa72bc784d4f4512d1ecbc1d241f2a3f9
Adminadmin <2312895350@qq.com>
2020-05-05 23:38:18 +0800

    123

---
+++ day01_demo01/src/main/java/com/itzhao/domain/User.java
@@ -0,0 +1,8 @@
+package com.itzhao.domain;
+
+/**
+ * com.itzhao.domain
+ * Created on 2020/5/5.
+ */
+public class User {
+}
---
+++ day01_demo01/src/main/java/com/itzhao/test/mytest.java
@@ -0,0 +1,12 @@
+package com.itzhao.test;
+
+/**
+ * com.itzhao.test
+ * Created on 2020/5/5.
+ */
+public class mytest {
+
+    public static void main(String[] args) {
+        System.out.println("hahahahah");
+    }
+}
